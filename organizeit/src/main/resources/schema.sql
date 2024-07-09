95f6027e356b1299c2aea83e20835f7aaff8ddd1
LeonSchampach <92578089+LeonSchampach@users.noreply.github.com>
2024-07-09 12:00:29 +0200

    Bug Fix
    Fixed a Bug where drawers with a space in the name would not be displayed correctly in the edit Shelf window

--- organizeit/src/main/resources/schema.sql
+++ organizeit/src/main/resources/schema.sql
@@ -2,21 +2,21 @@ DROP TABLE IF EXISTS item;
 DROP TABLE IF EXISTS drawer;
 DROP TABLE IF EXISTS shelf;
 
-CREATE TABLE /*IF NOT EXISTS*/ shelf (
+CREATE TABLE IF NOT EXISTS shelf (
                         id LONG NOT NULL AUTO_INCREMENT,
                         name VARCHAR(255) NOT NULL,
                         room VARCHAR(255),
                         PRIMARY KEY (id)
 );
 
-CREATE TABLE /*IF NOT EXISTS*/ drawer (
+CREATE TABLE IF NOT EXISTS drawer (
                         id LONG NOT NULL AUTO_INCREMENT,
                         name VARCHAR(255) NOT NULL,
                         shelf_id LONG,
                         PRIMARY KEY (id),
                         CONSTRAINT fk_shelf FOREIGN KEY (shelf_id) REFERENCES shelf(id)
 );
-CREATE TABLE /*IF NOT EXISTS*/ item (
+CREATE TABLE IF NOT EXISTS item (
                         id LONG NOT NULL AUTO_INCREMENT,
                         name VARCHAR(255) NOT NULL,
                         desc VARCHAR(255),
